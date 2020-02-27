96a8f123c995af789a0e96bc691a5595dd6cdd9e
Jessiley Oliveira <jessiley.oliveira@iv2.com.br>
2020-02-27 14:01:29 -0300

    Feat: added customer model

---
+++ src/models/customer.js
@@ -0,0 +1,17 @@
+const { Model, DataTypes } = require("sequelize");
+
+class Customer extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        name: DataTypes.STRING,
+        email: DataTypes.STRING
+      },
+      {
+        sequelize
+      }
+    );
+  }
+}
+
+module.exports = Customer;
